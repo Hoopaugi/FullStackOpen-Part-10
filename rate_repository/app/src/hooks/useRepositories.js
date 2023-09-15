87aaf06e15bbcfac8b5382587ed7df0180388682
Hoopaugi <hoopaugi@gmail.com>
2023-09-15 12:56:44 +0300

    Rate repo progress

---
+++ rate_repository/app/src/hooks/useRepositories.js
@@ -0,0 +1,11 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' });
+
+  return { repositories: data.repositories, loading, error, refetch };
+};
+
+export default useRepositories;
