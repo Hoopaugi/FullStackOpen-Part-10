f740d4d5af0e43c2f171316dc83fbd41aacc4744
Hoopaugi <hoopaugi@gmail.com>
2023-09-16 12:03:24 +0300

    Rate repo progress

--- rate_repository/example_app/src/components/RepositoryList.jsx
+++ rate_repository/example_app/src/components/RepositoryList.jsx
@@ -1,10 +1,32 @@
+import { useState, useRef } from 'react';
+import {Picker} from '@react-native-picker/picker';
+
 import useRepositories from '../hooks/useRepositories';
 import RepositoryListContainer from './RepositoryListContainer';
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [order, setOrder] = useState('latest')
 
-  return <RepositoryListContainer repositories={repositories} />
+  const pickerRef = useRef();
+
+  const { repositories } = useRepositories(order);
+
+  return (
+    <>
+      <Picker
+        ref={pickerRef}
+        selectedValue={order}
+        // eslint-disable-next-line no-unused-vars
+        onValueChange={(itemValue, itemIndex) =>
+          setOrder(itemValue)
+        }>
+        <Picker.Item label="Latest repositories" value="latest" />
+        <Picker.Item label="Highest rated repositories" value="highest" />
+        <Picker.Item label="Lowest rated repositories" value="lowest" />
+      </Picker>
+      <RepositoryListContainer repositories={repositories} />
+    </>
+  )
 };
 
 export default RepositoryList;
--- rate_repository/example_app/src/hooks/useRepositories.js
+++ rate_repository/example_app/src/hooks/useRepositories.js
@@ -2,9 +2,12 @@ import { useQuery } from '@apollo/client';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (order) => {
+  const orderBy = order === 'latest' ? 'CREATED_AT' : 'RATING_AVERAGE'
+  const orderDirection = order === 'latest' ? 'DESC' : order === 'highest' ? 'DESC' : 'ASC'
+
   const { data, ...result } = useQuery(GET_REPOSITORIES, {
-    fetchPolicy: 'cache-and-network',
+    fetchPolicy: 'cache-and-network', variables: { orderBy, orderDirection }
   });
 
   return { repositories: data ? data.repositories : undefined, ...result };
